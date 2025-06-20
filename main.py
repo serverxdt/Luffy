73d93451858fa364b157145a79c26f50b4c57c06
serverxdt <runningserver94@gmail.com>
2025-06-21 00:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ headers = {
 
 @app.route('/')
 def index():
-    return '''<!DOCTYPE html>
-<html lang="en">
+    return ' ' '
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
