0cc27e933be2d556fea0c9106ef86ecca539f8ff
serverxdt <runningserver94@gmail.com>
2025-07-18 21:38:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ HTML_TEMPLATE = """
             margin-top: 10px;
         }
         button:hover {
-            background: background: 45deg, #007e33, #00c851;
+            background: 45deg, #007e33, #00c851;
         }
         .result {
             margin-top: 20px;
